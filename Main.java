f42934999a4b4e3c2b22b9213a8430d1cbebbbd7
Sara Samer <ss.titanic199@gmail.com>
2020-12-24 11:23:01 +0200

    initial data

---
+++ Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Main
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Integer var = sc.nextInt();
+        Integer project_funding = sc.nextInt();
+        Integer team_experience_level = sc.nextInt();
+
+        
+
+    }
+}
