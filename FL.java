036f0de96539a7141af3f71929d7df6eb6f70665
salma27 <36279740+salma27@users.noreply.github.com>
2020-12-25 15:58:08 +0200

    project funding class added

--- FL.java
+++ FL.java
@@ -1,3 +1,16 @@
 public class FL {
+    private float targetProjectFunding;
+    private float targetTeamExperience;
+	
+    public FL() {
+    	targetProjectFunding = 0;
+    	targetTeamExperience = 0;
+    }
     
+	public FL(float projectFunding, float teamExperience) {
+		targetProjectFunding = projectFunding;
+		targetTeamExperience = teamExperience;
+	}
+	
+	
 }
