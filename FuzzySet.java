3962f8596027aa6854591886fcdac032ee1aefc5
salma27 <36279740+salma27@users.noreply.github.com>
2020-12-26 14:03:43 +0200

    fuzzySet class added

---
+++ FuzzySet.java
@@ -0,0 +1,12 @@
+public class FuzzySet{
+	private String name;
+	
+	
+	public FuzzySet() {
+		name = "";
+	}
+	public FuzzySet(String name) {
+		this.name = name;
+	}
+	
+}
--- Main.java
+++ Main.java
@@ -8,13 +8,18 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Variables: ");
         Integer var = sc.nextInt();
-        System.out.print("Project Fund: ");
+        for(int i = 0 ; i < 2 ; i++) {
+        	String fuzzySetName = sc.next();
+        	int fuzzySetOut = sc.nextInt();
+        }
+        
+        /*System.out.print("Project Fund: ");
         Float project_funding = sc.nextFloat();
         System.out.print("Experience Level: ");
         Float team_experience_level = sc.nextFloat();
         
         FL fuzzy = new FL(project_funding, team_experience_level);
-        
+        */
 
     }
 }
