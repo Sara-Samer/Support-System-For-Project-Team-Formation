2f95fadf71f52ae3306d6f1355a2d24e4b9f7165
salma27 <36279740+salma27@users.noreply.github.com>
2020-12-26 17:06:34 +0200

    fuzzy set added

--- FuzzySet.java
+++ FuzzySet.java
@@ -1,24 +1,87 @@
+import java.util.HashMap;
 import java.util.Map;
-import java.util.Vector;
 
 public class FuzzySet{
 	private String name;
 	private float target;
-	private Map<String, Vector<Float>> sets;
+	private Map<String, float[]> sets;
 	
 	public FuzzySet() {
 		name = "";
 		target = 0;
-		sets = null;
+		sets = new HashMap<>();
 	}
 	
 	public FuzzySet(String name, Float target) {
 		this.name = name;
 		this.target = target;
+		sets = new HashMap<>();
 	}
 	
-	public void addSet(String set, Vector<Float> points) {
+	public void addSet(String set, float[] points) {
 		sets.put(set, points);
 	}
 	
+	public float geta(float x1, float y1, float x2, float y2) {
+		return ((y2 - y1) / (x2 - x1));
+	}
+	
+	public float getb(float x , float y, float a) {
+		return y - (a * x);
+	}
+	
+	public float calculateY(float x, float[] line) {
+		float y = 0;
+		float x1, y1, x2, y2;
+		for(int i = 0; i < line.length; i++) {
+			if(i + 1 < line.length && x >= line[i] && x <= line[i + 1] ) {
+				x1 = line[i];
+				x2 = line[i+1];
+				if(i == 0) {
+					y1 = 0;
+					y2 = 1;
+				}
+				else if(i + 1 == line.length - 1) {
+					y1 = 1;
+					y2 = 0;
+				}
+				else {
+					y1 = 1;
+					y2 = 1;
+				}
+				float a = geta(x1, y1, x2, y2);
+				y = (a * x) + getb(x1, y1, a);
+			}
+		}
+		return y;
+	}
+	
+	public Map<String, Float> getRange(){
+		Map<String, Float> lineIntersection = new HashMap<String, Float>();
+		String key = "";
+		for(int i = 0 ; i < sets.size(); i++) {
+			key = (String) sets.keySet().toArray()[i];
+			if(target >= sets.get(key)[0] && target <= sets.get(key)[sets.get(key).length - 1]) {
+				lineIntersection.put(key, calculateY(target, sets.get(key)));
+			}
+			else {
+				lineIntersection.put(key, (float) 0);
+			}
+		}
+		return lineIntersection;
+	}
+	public void printSets() {
+		System.out.println(name);
+		System.out.println(target);
+		System.out.println("-------------:");
+		for(int i = 0 ; i < sets.size(); i++) {
+			System.out.print(sets.keySet().toArray()[i] + "  {");
+			for(int j = 0 ; j < sets.get(sets.keySet().toArray()[i]).length; j++) {
+				System.out.print(sets.get(sets.keySet().toArray()[i])[j] + " , ");
+			}
+			System.out.println("}");
+		}
+		System.out.println("-------------------------------------");
+		
+	}
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Vector;
 
 /**
  * Main
@@ -8,10 +10,48 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.print("Variables: ");
         Integer var = sc.nextInt();
-        for(int i = 0 ; i < 2 ; i++) {
+        Vector<FuzzySet> fuzzy = new Vector<>();
+        for(int i = 0 ; i < var ; i++) {
         	String fuzzySetName = sc.next();
-        	int fuzzySetOut = sc.nextInt();
+        	Float fuzzySetOut = sc.nextFloat();
+        	FuzzySet set = new FuzzySet(fuzzySetName, fuzzySetOut);
+        	fuzzy.add(set);
         }
+        float[] arr = {0, 0, 10, 30};
+        fuzzy.elementAt(0).addSet("vl", arr );
+        float[] arr2 = {10, 30, 40, 60};
+        fuzzy.elementAt(0).addSet("l", arr2 );
+        float[] arr3 = {40, 60, 70, 90};
+        fuzzy.elementAt(0).addSet("m", arr3 );
+        float[] arr4 = {70, 90, 100, 100};
+        fuzzy.elementAt(0).addSet("h", arr4 );
+        
+        float[] arr5 = {0, 15, 30};
+        fuzzy.elementAt(1).addSet("b", arr5 );
+        float[] arr6 = {15, 30, 45};
+        fuzzy.elementAt(1).addSet("i", arr6 );
+        float[] arr7 = {30, 60, 60};
+        fuzzy.elementAt(1).addSet("e", arr7 );
+        
+        
+        //fuzzy.elementAt(0).printSets();
+        //fuzzy.elementAt(1).printSets();
+        Vector<Map<String, Float>> allRanges = new Vector<>();
+        for(int i = 0 ; i < fuzzy.size(); i++) {
+        	allRanges.add(fuzzy.elementAt(i).getRange());
+        }
+        
+        String key = "";
+        for(int i = 0 ; i < allRanges.size(); i++) {
+        	for(int j = 0 ; j < allRanges.elementAt(i).keySet().size(); j++) {
+        		key = (String) allRanges.elementAt(i).keySet().toArray()[j];
+        		System.out.println(key + "  =  " + allRanges.elementAt(i).get(key));
+        	}
+        }
+        
+       //-------------------------------------------------------------------------------------------
+        
+        
         
         /*System.out.print("Project Fund: ");
         Float project_funding = sc.nextFloat();
