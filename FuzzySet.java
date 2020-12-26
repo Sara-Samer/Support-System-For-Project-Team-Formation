72765133eaa0d1ecf2ff8e5896f58b8a272893f8
salma27 <36279740+salma27@users.noreply.github.com>
2020-12-26 14:23:51 +0200

    Update FuzzySet.java

--- FuzzySet.java
+++ FuzzySet.java
@@ -1,12 +1,24 @@
+import java.util.Map;
+import java.util.Vector;
+
 public class FuzzySet{
 	private String name;
-	
+	private float target;
+	private Map<String, Vector<Float>> sets;
 	
 	public FuzzySet() {
 		name = "";
+		target = 0;
+		sets = null;
 	}
-	public FuzzySet(String name) {
+	
+	public FuzzySet(String name, Float target) {
 		this.name = name;
+		this.target = target;
+	}
+	
+	public void addSet(String set, Vector<Float> points) {
+		sets.put(set, points);
 	}
 	
 }
